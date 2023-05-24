aa1865221a7f146c132afb0153b448a8c38a1a2d
Eleomar Carvalho <eleomarscarvalho@gmail.com>
2023-05-24 10:00:27 -0300

    Projeto LH-Games

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,20 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RestritoRoutingModule } from './restrito/restrito-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { RestritoComponent } from './restrito/restrito.component';
+import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
+import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
+import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
+import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,6 +34,13 @@ import { RodapeComponent } from './rodape/rodape.component';
     MenuComponent,
     LoginComponent,
     RodapeComponent,
+    RestritoComponent,
+    AtualizaProdutoComponent,
+    CadastroProdutoComponent,
+    ListaProdutoComponent,
+    MenuRestritoComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
@@ -39,6 +54,10 @@ import { RodapeComponent } from './rodape/rodape.component';
     MatInputModule,
     MatMenuModule,
     MatToolbarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RestritoRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/restrito/lista-produto/lista-produto.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { Produto } from 'src/app/models/produto.model';
+import { ProdutoService } from 'src/app/produto.service';
+import { LoginService } from 'src/app/login.service';
+
+@Component({
+  selector: 'app-lista-produto',
+  templateUrl: './lista-produto.component.html',
+  styleUrls: ['./lista-produto.component.css']
+})
+export class ListaProdutoComponent implements OnInit {
+
+  public produtos: Produto[] = [];
+
+  constructor(private _produtoService: ProdutoService, private _router: Router, 
+    private _loginService:LoginService) {}
+
+  ngOnInit(): void {
+    this.listarProdutos();
+    this._loginService.setMostraMenu(false);
+  }
+
+  listarProdutos(): void {
+    this._produtoService.getProdutos().subscribe(
+      retornaProduto => {
+        this.produtos = retornaProduto.map(
+          item => {
+            return new Produto(
+              item.id,
+              item.produto,
+              item.descricao,
+              item.foto,
+              item.preco,
+            );
+          }
+        )
+      }
+    )
+  }
+
+  excluir(id: number){
+    this._produtoService.removerProduto(id).subscribe(
+      produto => {
+        this.listarProdutos();
+      },
+      err => { alert("Erro ao Excluir") }
+    );
+
+    this._router.navigate(["/restrito/lista"]);
+
+  }
+
+  
+}
